d60fab6a4c428db8df1f4c2dedf9771e166563a9
Bruce <brucepark0@gmail.com>
2017-07-15 16:23:35 -0700

    Add Docker config

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Allthingselixir.Mixfile do
   def project do
     [app: :allthingselixir,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
