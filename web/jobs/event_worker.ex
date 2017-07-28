f3c09c64487038b4cdb369fdcf4076a7fbcc142f
Bruce <brucepark0@gmail.com>
2017-07-27 22:57:49 -0700

    Read in date and datetimes

--- web/jobs/event_worker.ex
+++ web/jobs/event_worker.ex
@@ -12,7 +12,7 @@ defmodule Allthingselixir.EventWorker do
   end
 
   def handle_info(:work, state) do
-    current_year = (Calendar.DateTime.now! "UTC").year
+    current_year = Timex.now.year
     data = File.cwd!
            |> Path.join("web/data/#{current_year}_conferences.yml")
            |> YamlElixir.read_from_file
@@ -33,6 +33,33 @@ defmodule Allthingselixir.EventWorker do
 #   "start_date" => "09/05/2017", "time_zone" => "PST",
 #   "twitter" => "ElixirConf", "url" => "https://elixirconf.com/"}]
     data
-    |> Enum.each(fn(opts) -> Event.find_or_create_by_name(opts["name"], Map.drop(opts, ["location", "name", "start_date", "end_date", "time_zone", "cfp_closing_datetime"])) end)
+    |> Enum.map(fn(map) -> process_map_with_dates(map) end)
+    |> Enum.each(fn(opts) -> Event.find_or_create_by_name(opts["name"], Map.drop(opts, ["name"])) end)
   end
+
+  def process_map_with_dates(map) do
+    %{"cfp_closing_datetime" => cfp_closing_datetime,
+      "start_date" => start_date, "end_date" => end_date } = map
+    Map.merge(map, %{"cfp_closing_datetime" => transform_datetime(cfp_closing_datetime),
+      "start_date" => transform_date(start_date),
+      "end_date" => transform_date(end_date)}
+    )
+  end
+
+  def transform_datetime(input) do
+    # {:ok, ~N[2016-09-05 23:59:00]}
+    {status, datetime} = Timex.parse(input, "%m/%d/%Y %I:%M %p", :strftime)
+    transform_datetime(status, datetime, input)
+  end
+
+  def transform_datetime(:error, datetime_str, original_input), do: original_input
+  def transform_datetime(:ok, datetime_str, original_input), do: datetime_str
+
+  def transform_date(input) do
+    {status, date} = Timex.parse(input, "{0M}/{0D}/{YYYY}")
+    transform_date(status, date, input)
+  end
+
+  def transform_date(:error, date_str, original_input), do: original_input
+  def transform_date(:ok, date_str, original_input), do: date_str
 end
